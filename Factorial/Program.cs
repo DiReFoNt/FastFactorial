c70a90c7025775dcf4d80b8f2e13e8b25afd6b72
Alexandr <86528328+DiReFoNt@users.noreply.github.com>
2021-07-11 15:23:49 +0300

    FastFactorial
    c#

---
+++ Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int TrailingZeros(int n)
+        {
+            int result = 0;
+            var factorial = new BigInteger(1);
+            int j = 1;
+            while (j <= n)
+            {
+                factorial *= j;
+                ++j;
+            }
+            Console.WriteLine(factorial);
+            string str = Convert.ToString(factorial);
+            char[] ch = str.ToCharArray();
+            Array.Reverse(ch);
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] != '0')
+                    break;
+                if (ch[i] == '0')
+                    ++result;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TrailingZeros(105423));
+        }
+    }
+}
